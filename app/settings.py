bd78673060ed1d4e19624ebd151a55a7e1fc6ffe
Lucas Cardoso RIbeiro <97406660+LucasRib52@users.noreply.github.com>
2025-08-28 22:04:23 -0300

    atualizacao pra melhora do dashboard e passar pro mysql2

--- app/settings.py
+++ app/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=+p$5yp11)h9a)aem+gtg*$n@9i5=mb48w$+jyuxuhvjl*#8os'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
